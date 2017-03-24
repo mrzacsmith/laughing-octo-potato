ed0bf0aae9da94a888444875854482775d4413d0
Zac Smith <zrs3141592@gmail.com>
2017-03-24 15:01:29 -0600

    Build to_do, doing, done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170324202158) do
+ActiveRecord::Schema.define(version: 20170324203843) do
 
   create_table "tashes", force: :cascade do |t|
     t.text     "content"
@@ -20,9 +20,10 @@ ActiveRecord::Schema.define(version: 20170324202158) do
 
   create_table "tasks", force: :cascade do |t|
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
     t.integer  "user_id"
+    t.string   "state",      default: "to_do"
   end
 
   create_table "users", force: :cascade do |t|
