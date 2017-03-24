69c8d402f030eaa7fb042a6f4c7d3498c99a5b5d
Zac Smith <zrs3141592@gmail.com>
2017-03-24 14:37:44 -0600

    Add task associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170324195910) do
+ActiveRecord::Schema.define(version: 20170324202158) do
 
   create_table "tashes", force: :cascade do |t|
     t.text     "content"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20170324195910) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
